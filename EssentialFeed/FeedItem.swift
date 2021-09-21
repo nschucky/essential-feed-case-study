36d84533193bbddaebbf2cc91ff6290c3aec4b6f
Antonio Alves <antonioalvesdev@gmail.com>
2021-09-20 17:09:21 -0700

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Antonio Alves on 9/20/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
