f8845c229e95225e7d3136577ac75a9a1a0f3af4
Antonio Alves <antonioalvesdev@gmail.com>
2021-12-09 11:56:18 -0800

    Extract memory leak tracking helper method to a shared extension on XCTestCase

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -118,12 +118,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         return RemoteFeedLoader.Result.failure(error)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
-        }
-    }
-    
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL = URL(string: "https://any-image.com")!) -> (model: FeedItem, json: [String: Any]) {
         let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
         let json = [
