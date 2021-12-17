e111ca58f3015b693a2c5b3fc940a5eb1fd46c67
Antonio Alves <antonioalvesdev@gmail.com>
2021-12-17 12:48:50 -0800

    Finish URL loading when observing requests to make sure all URL requests are finished before the test method returns. This way, we prevent data races with threads living longer than the test method that initiated them

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -10,15 +10,12 @@ import EssentialFeed
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        
+    override func setUpWithError() throws {
         URLProtocolStub.startInterceptingRequests()
+
     }
-        
-    override class func tearDown() {
-        super.tearDown()
-        
+
+    override func tearDownWithError() throws {
         URLProtocolStub.stopInterceptingRequests()
     }
     
@@ -29,11 +26,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
-        }
-        
-        makeSUT().get(from: url) { _ in
             exp.fulfill()
         }
+        
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -162,15 +158,6 @@ class URLSessionHTTPClientTests: XCTestCase {
             stub = Stub(data: data, response: response, error: error)
         }
         
-        override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
-            return true
-        }
-        
-        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-            return request
-        }
-        
         static func startInterceptingRequests() {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
@@ -181,7 +168,20 @@ class URLSessionHTTPClientTests: XCTestCase {
             requestObserver = nil
         }
         
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
         override func startLoading() {
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
+            
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
